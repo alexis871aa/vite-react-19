a5fe3532506fcf47f5637a63c3b49241668e0739
Aleksey Sokolov <alexis871aa@yandex.ru>
2024-12-04 22:28:30 +0300

    Продолжаем изучать React 19 14:00

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,41 @@
-import { useState } from "react";
+import { useState, useActionState } from "react";
 import { loginUser } from "./api/user";
 
 export const App = () => {
-  // Теперь состояния username и password не нужны, они берутся из объекта formData
-  // const [username, setUsername] = useState("");
-  // const [password, setPassword] = useState("");
-  const [user, setUser] = useState(null);
-  const [error, setError] = useState(null);
-  const [isPending, setIsPending] = useState(false);
+  const [state, submitAction, isPending] = useActionState(handleAction, {
+    user: null,
+    error: null,
+  });
 
-  const handleSubmit = async (formData) => {
-    setIsPending(true);
-    setUser(null);
-    setError(null);
-
+  async function handleAction(prevState, formData) {
     const username = formData.get("username");
     const password = formData.get("password");
-
     try {
       const response = await loginUser(username, password);
-      setUser(response.data);
+      return { user: response.data, error: null };
     } catch (error) {
-      setError(error.error);
-    } finally {
-      setIsPending(false);
+      return { ...prevState, error: error.error };
     }
-
-    // КАК БЫЛО РАНЬШЕ!!!
-    // e.preventDefault();
-    //
-    // setIsPending(true);
-    // setUser(null);
-    // setError(null);
-    //
-    // try {
-    //   const response = await loginUser(username, password);
-    //   setUser(response.data);
-    // } catch (error) {
-    //   setError(error.error);
-    // } finally {
-    //   setIsPending(false);
-    // }
-  };
+  }
 
   return (
-    <form action={handleSubmit}>
+    <form action={submitAction}>
       <div>
         <label>Username:</label>
-        <input
-          type="text"
-          name="username"
-          // value={formData.username}
-          // onChange={(e) => setUsername(e.target.value)}
-          required
-        />
+        <input type="text" name="username" required />
       </div>
       <div>
         <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          // value={password}
-          // onChange={(e) => setPassword(e.target.value)}
-          required
-        />
+        <input type="password" name="password" required />
       </div>
       <button type="submit" disabled={isPending}>
         {isPending ? "Logging in..." : "Login"}
       </button>
 
-      {user && <p style={{ color: "green" }}>Logged in: {user.email}</p>}
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      {state.user && (
+        <p style={{ color: "green" }}>Logged in: {state.user.email}</p>
+      )}
+      {state.error && <p style={{ color: "red" }}>{state.error}</p>}
     </form>
   );
 };
